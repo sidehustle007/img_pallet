6ef51bb4edb48fbb028cbe421b98c3e4564645ef
naolbm <profesornaoltena@gmail.com>
2024-03-23 13:17:28 +0300

    update on color palette look

--- src/App.jsx
+++ src/App.jsx
@@ -54,119 +54,190 @@ const App = () => {
   }, [imgColors]);
 
   return (
-    <center>
-      <h2>Image Selector</h2>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-
-      {selectedImage && (
-        <>
-          <div>
-            <ColorExtractor getColors={(colors) => setImgColors(colors)}>
-              <img
-                onClick={() => setRgbTF(!rgbtf)}
-                className="select_img"
-                src={selectedImage}
-                alt="Selected"
-                style={{
-                  // border: `5px rgb(${cr}, ${cg}, ${cb}) solid`,
-                  borderRadius: "20px",
-                  boxShadow: `2px 12px 125px rgba(${cr}, ${cg}, ${cb}, 1)`,
-                }}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        width: "100%",
+        height: "100vh",
+        gap: "50px",
+      }}
+      className="main_bg"
+    >
+      {!selectedImage ? (
+        <div
+          style={{
+            top: "0px",
+          }}
+          className="title"
+        >
+          Color Pallet
+        </div>
+      ) : (
+        <div
+          className="nav_bar_title"
+          style={{ boxShadow: `2px 12px 125px rgba(${cr}, ${cg}, ${cb}, 1)` }}
+        >
+          <div className="title_nav">Color Pallet</div>
+        </div>
+      )}
+      {!selectedImage ? (
+        <div class="container">
+          <div class="card">
+            <h3>Upload Image</h3>
+            <label class="drop_box" for="file-upload">
+              <header>
+                <h4>Drag Image File Here</h4>
+              </header>
+              <p>Files Supported: JPEG, PNG</p>
+              <input
+                id="file-upload"
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                className="input_customize"
               />
-            </ColorExtractor>
+              <label for="file-upload" className="select_img_lable">
+                <div className="select_img_la">Select IMG</div>
+              </label>
+            </label>
+          </div>
+        </div>
+      ) : (
+        <div style={{ position: "fixed", bottom: "15px" }}>
+          <label for="file-upload" class="upload-custone-2">
+            <img
+              className="img_upload"
+              src="https://cdn.iconscout.com/icon/free/png-256/free-upload-1851088-1569238.png"
+              alt=""
+            />
+            {/* <h4 className="upload_txt_img">Select IMG</h4> */}
+          </label>
+          <input
+            id="file-upload"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="input_customize"
+          />
+        </div>
+      )}
+      <center>
+        {selectedImage && (
+          <>
+            <div>
+              <ColorExtractor getColors={(colors) => setImgColors(colors)}>
+                <img
+                  onClick={() => setRgbTF(!rgbtf)}
+                  className="select_img"
+                  src={selectedImage}
+                  alt="Selected"
+                  style={
+                    {
+                      // border: `5px rgb(${cr}, ${cg}, ${cb}) solid`,
+                      // borderRadius: "20px",
+                      // boxShadow: `2px 12px 125px rgba(${cr}, ${cg}, ${cb}, 1)`,
+                    }
+                  }
+                />
+              </ColorExtractor>
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: "1px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: "20px",
+                }}
+              >
+                {imgColors?.map((hexcode, index) => (
+                  <div
+                    style={{
+                      backgroundColor: hexcode,
+                      minWidth: "10vh",
+                      height: "3vh",
+                      padding: "20px",
+                      display: "flex",
+                      // borderRadius: "5px",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      color: "white",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => getNumbersBeforeComma(hex2rgb(hexcode))}
+                  >
+                    {rgbtf ? hexcode : hex2rgb(hexcode)}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div
+              style={{
+                backgroundColor: `rgb(${cr},${cg}, ${cb})`,
+                width: "20vh",
+                minHeight: "7vh",
+                padding: "20px",
+                marginTop: "20px",
+                display: "flex",
+                textAlign: "initial",
+                borderRadius: "5px",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+              }}
+            >
+              R = {cr}({calculatePercentage(cr)}%)
+              <br /> G = {cg}({calculatePercentage(cg)}%)
+              <br />B = {cb}({calculatePercentage(cb)}%)
+            </div>
             <div
               style={{
                 display: "flex",
-                flexWrap: "wrap",
-                gap: "1px",
-                justifyContent: "center",
-                alignItems: "center",
                 marginTop: "20px",
+                // transform: "rotate(-90deg)",
+                flexDirection: "column",
+                height: "10vh",
+                width: "20vh",
+                // marginLeft: "-40px",
+                gap: "10px",
               }}
             >
-              {imgColors?.map((hexcode, index) => (
-                <div
-                  style={{
-                    backgroundColor: hexcode,
-                    width: "10vh",
-                    height: "3vh",
-                    padding: "20px",
-                    display: "flex",
-                    // borderRadius: "5px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    color: "white",
-                    cursor: "pointer",
-                  }}
-                  onClick={() => getNumbersBeforeComma(hex2rgb(hexcode))}
-                >
-                  {rgbtf ? hexcode : hex2rgb(hexcode)}
-                </div>
-              ))}
+              <input
+                type="range"
+                onChange={(e) => setR(e.target.value)}
+                name="colorRange"
+                min="0"
+                value={cr}
+                max="255"
+                className="cr"
+              />
+              <input
+                type="range"
+                className="cg"
+                onChange={(e) => setG(e.target.value)}
+                name="colorRange"
+                value={cg}
+                min="0"
+                max="255"
+              />
+              <input
+                className="cb"
+                type="range"
+                onChange={(e) => setB(e.target.value)}
+                name="colorRange"
+                value={cb}
+                min="0"
+                max="255"
+              />
             </div>
-          </div>
-
-          <div
-            style={{
-              backgroundColor: `rgb(${cr},${cg}, ${cb})`,
-              width: "20vh",
-              height: "7vh",
-              padding: "20px",
-              marginTop: "20px",
-              display: "flex",
-              borderRadius: "5px",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "white",
-            }}
-          >
-            R = {cr}({calculatePercentage(cr)}%)
-            <br /> G = {cg}({calculatePercentage(cg)}%)
-            <br />B = {cb}({calculatePercentage(cb)}%)
-          </div>
-          <div
-            style={{
-              display: "flex",
-              marginTop: "20px",
-              // transform: "rotate(-90deg)",
-              flexDirection: "column",
-              height: "10vh",
-              width: "20vh",
-              // marginLeft: "-40px",
-              gap: "10px",
-            }}
-          >
-            <input
-              type="range"
-              onChange={(e) => setR(e.target.value)}
-              name="colorRange"
-              min="0"
-              value={cr}
-              max="255"
-              className="cr"
-            />
-            <input
-              type="range"
-              className="cg"
-              onChange={(e) => setG(e.target.value)}
-              name="colorRange"
-              value={cg}
-              min="0"
-              max="255"
-            />
-            <input
-              className="cb"
-              type="range"
-              onChange={(e) => setB(e.target.value)}
-              name="colorRange"
-              value={cb}
-              min="0"
-              max="255"
-            />
-          </div>
-        </>
-      )}
-    </center>
+          </>
+        )}
+      </center>
+    </div>
   );
 };
 
